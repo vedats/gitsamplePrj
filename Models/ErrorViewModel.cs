71cddf3bc716b93287ad0b8d8456fda2be641885
vedat sebzeci <vedat@gelisim.net>
2020-09-17 12:29:40 +0300

    2.nd Upload

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace gitsamplePrj.Models
 {
     public class ErrorViewModel
     {
+        public int Id { get; set; }
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
